2ed2245a2298cebe2e00ffd2480b0a7555e569df
gabriel andrin <gabriela@theodo.fr>
2020-06-18 12:20:32 +0200

    Creates Sequence module

---
+++ src/sequence/sequence.schema.ts
@@ -0,0 +1,8 @@
+import * as mongoose from 'mongoose';
+
+export const sequenceSchema = new mongoose.Schema({
+  sequenceName: String,
+  sequenceValue: Number,
+});
+
+mongoose.model('Sequence', sequenceSchema);
