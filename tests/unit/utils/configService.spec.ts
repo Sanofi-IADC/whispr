75da6a88c16b69321af96d15e0f5c5f695ed729b
Wennemaring <David.Wennemaring-ext@sanofi.com>
2020-11-05 10:43:36 +0100

    test(proxy-test): added tests for proxy configuration in Config-Service

---
+++ tests/unit/utils/configService.spec.ts
@@ -0,0 +1,52 @@
+import { ConfigService } from '../../../src/config/config.service';
+
+describe('ConfigService', () => {
+  let configService: ConfigService;
+  beforeEach(() => {
+    process.env.NODE_ENV = 'example';
+  });
+  describe('get', () => {
+    beforeEach(() => {
+      process.env.HTTPS_PROXY = 'http://test.proxxy.com:3128';
+      configService = new ConfigService();
+    });
+    it('should return var from process.env', () => {
+      const proxy = configService.get('HTTPS_PROXY');
+      expect(proxy).toEqual(process.env.HTTPS_PROXY);
+    });
+    it('should return var from config-file', () => {
+      const proxy = configService.get('MONGOOSE_HOST');
+      expect(proxy).toEqual('mongo1');
+    });
+    it('should return undefined if var is not set', () => {
+      const result = configService.get('HTTP_PROXY');
+      expect(result).toBeUndefined();
+    });
+  });
+  describe('getProxyConfig', () => {
+    describe('with proxy set', () => {
+      beforeEach(() => {
+        process.env.HTTPS_PROXY = 'http://test.proxxy.com:3128';
+        configService = new ConfigService();
+      });
+      it('should return proxy config', () => {
+        const expectedResult = {
+          host: 'test.proxxy.com',
+          port: 3128,
+        };
+        const result = configService.getProxyConfig();
+        expect(result).toEqual(expectedResult);
+      });
+    });
+    describe('without proxy set', () => {
+      beforeEach(() => {
+        delete process.env.HTTPS_PROXY;
+        configService = new ConfigService();
+      });
+      it('should return undefined', () => {
+        const result = configService.getProxyConfig();
+        expect(result).toBeUndefined();
+      });
+    });
+  });
+});
