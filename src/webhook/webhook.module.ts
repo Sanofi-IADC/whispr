f513f857017718eaf18b855f9f7f8e1229bbd9e1
Wennemaring <David.Wennemaring-ext@sanofi.com>
2020-11-05 09:53:37 +0100

    fix(webhooks): set https-Agent to correctly use Proxy & CA_Certificate for Webhook calls

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,15 +1,35 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import * as tunnel from 'tunnel';
+import { ConfigModule } from '../config/config.module';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
 import { webhookSchema } from './webhook.schema';
 import { WebhookResolver } from './webhook.resolver';
+import { ConfigService } from '../config/config.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Webhook', schema: webhookSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Webhook', schema: webhookSchema }]),
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        proxy: false,
+        httpsAgent: tunnel.httpsOverHttp({
+          ca: configService.get('CA_CERTIFICATE') ? [Buffer.from(configService.get('CA_CERTIFICATE'))] : undefined,
+          proxy: configService.getProxyConfig(),
+        }),
+      }),
+    }),
+  ],
   controllers: [WebhookController],
-  providers: [WebhookService, WebhookResolver],
+  providers: [
+    WebhookService,
+    WebhookResolver,
+    ConfigService,
+  ],
   exports: [WebhookService],
 })
-export class WebhookModule {}
+export class WebhookModule { }
