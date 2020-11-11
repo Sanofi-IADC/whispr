80e02d57f891f021124270fe643994217ac552ac
Thomas WALTER <twalter@lumiomedical.com>
2020-11-11 10:56:36 +0100

    test(e2e): test createWhisp GraphQL mutation

--- tests/e2e/app.e2e-spec.ts
+++ tests/e2e/app.e2e-spec.ts
@@ -1,27 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import request from 'supertest';
 import { INestApplication } from '@nestjs/common';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from '../../src/app.module';
 
 describe('AppController (e2e)', () => {
-  // let app: INestApplication;
+  let app: INestApplication;
 
-  // beforeEach(async () => {
-  //   const moduleFixture: TestingModule = await Test.createTestingModule({
-  //     imports: [AppModule],
-  //   }).compile();
-
-  //   app = moduleFixture.createNestApplication();
-  //   await app.init();
-  //   return;
-  // });
-
-  it('/ (GET)', () => {
-    expect(true).toBe(true);
-
-    // await request(app.getHttpServer())
-    //   .get('/')
-    //   .expect(200)
-    //   .expect('Hello World!');
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
+    await app.init();
+    await app.getHttpAdapter().getInstance().ready();
   });
+
+  afterAll(async () => {
+    await Promise.all([app.close()]);
+  });
+
+  it('/ (GET)', async () => await request(app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
 });
