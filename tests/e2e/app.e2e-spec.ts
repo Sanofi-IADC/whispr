ca886cc016c226405af991600f56ad5ba63b4bc6
Thomas WALTER <twalter@lumiomedical.com>
2020-11-12 15:26:25 +0100

    refactor(e2e): setup e2e test in a single file

--- tests/e2e/app.e2e-spec.ts
+++ tests/e2e/app.e2e-spec.ts
@@ -1,24 +1,5 @@
-import { Test } from '@nestjs/testing';
 import request from 'supertest';
-import { INestApplication } from '@nestjs/common';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { AppModule } from '../../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
-    await app.init();
-    await app.getHttpAdapter().getInstance().ready();
-  });
-
-  afterAll(async () => {
-    await Promise.all([app.close()]);
-  });
-
-  it('/ (GET)', async () => await request(app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
+  it('/ (GET)', async () => await request(global.app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
 });
