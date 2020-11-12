6c672a20fc6d59bfbf2e7f4136f76ae6aa365ef6
Thomas WALTER <twalter@lumiomedical.com>
2020-11-12 15:26:25 +0100

    refactor(e2e): setup e2e test in a single file

--- tests/e2e/whisp/graphql.e2e-spec.ts
+++ tests/e2e/whisp/graphql.e2e-spec.ts
@@ -1,8 +1,4 @@
-import { Test } from '@nestjs/testing';
 import request from 'supertest';
-import { INestApplication } from '@nestjs/common';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { AppModule } from '../../../src/app.module';
 
 const CREATE_WHISP_GQL = `
 mutation createWhisp($whisp: WhispInputType!) {
@@ -13,24 +9,9 @@ mutation createWhisp($whisp: WhispInputType!) {
 `;
 
 describe('GRAPHQL WhispModule (e2e)', () => {
-  let app: INestApplication;
-
-  beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
-    await app.init();
-    await app.getHttpAdapter().getInstance().ready();
-  });
-
-  afterAll(async () => {
-    await Promise.all([app.close()]);
-  });
-
   describe('createWhisp', () => {
     it('should returns a 200 and create a new Whisp and return an id', async () => {
-      const result = await request(app.getHttpServer())
+      const result = await request(global.app.getHttpServer())
         .post('/graphql')
         .send({
           query: CREATE_WHISP_GQL,
