69505e4626c7c81d3d3b3e8db534539e0c8e12f5
Thomas WALTER <twalter@lumiomedical.com>
2020-11-16 11:02:45 +0100

    test(e2e): test webhooks on whisp event
    also delete created whisps with tests

--- tests/e2e/whisp/graphql.e2e-spec.ts
+++ tests/e2e/whisp/graphql.e2e-spec.ts
@@ -1,3 +1,6 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { IWhisp } from 'src/interfaces/whisp.interface';
 import request from 'supertest';
 
 const CREATE_WHISP_GQL = `
@@ -8,15 +11,26 @@ mutation createWhisp($whisp: WhispInputType!) {
 }
 `;
 
+const WHISP_TEST_TYPE = 'E2E_TEST';
+
+afterAll(async () => {
+  try {
+    const model = global.app.get<Model<IWhisp>>(getModelToken('Whisp'));
+    await model.deleteMany({ type: WHISP_TEST_TYPE });
+  } catch {}
+});
+
 describe('GRAPHQL WhispModule (e2e)', () => {
   describe('createWhisp', () => {
-    it('should returns a 200 and create a new Whisp and return an id', async () => {
+    it('should return a 200 and create a new Whisp and return its id', async () => {
       const result = await request(global.app.getHttpServer())
         .post('/graphql')
         .send({
           query: CREATE_WHISP_GQL,
           variables: {
-            whisp: {},
+            whisp: {
+              type: WHISP_TEST_TYPE,
+            },
           },
         });
 
