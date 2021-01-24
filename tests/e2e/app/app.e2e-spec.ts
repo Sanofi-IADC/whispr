4f93b5fe1928d93ba7b9c42fd5cf424add4ab3b4
MeStrak <call_me_nigel@yahoo.co.uk>
2021-01-24 23:11:22 +0000

    test: fix linting rules for tests and move gql health test

--- tests/e2e/app/app.e2e-spec.ts
+++ tests/e2e/app/app.e2e-spec.ts
@@ -2,4 +2,8 @@ import request from 'supertest';
 
 describe('AppController (e2e)', () => {
   it('/ (GET)', async () => request(global.app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
+  it('Returns healthy graphQL status', async () => {
+    const res = await request(global.app.getHttpServer()).get('/.well-known/apollo/server-health');
+    expect(res['res'].statusCode).toBe(200); // eslint-disable-line
+  });
 });
--- tests/e2e/health/health.e2e-spec.ts
+++ tests/e2e/health/health.e2e-spec.ts
@@ -1,12 +1,8 @@
 import request from 'supertest';
 
-describe('AppController (e2e)', () => {
+describe('HealthController (e2e)', () => {
   it('Returns healthy status', async () => {
     const res = await request(global.app.getHttpServer()).get('/health');
-    expect(res.res.statusCode).toBe(200);
-  });
-  it('Returns healthy graphQL status', async () => {
-    const res = await request(global.app.getHttpServer()).get('/.well-known/apollo/server-health');
-    expect(res.res.statusCode).toBe(200);
+    expect(res['res'].statusCode).toBe(200); // eslint-disable-line dot-notation
   });
 });
