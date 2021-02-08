6a6d96c695c1c9a87e32ff7099bda4ae81fd2a43
MeStrak <call_me_nigel@yahoo.co.uk>
2021-02-08 21:48:13 +0000

    fix(instana): force import of instana first

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
+/* eslint-disable import/first */
+if (process.env.NODE_ENV !== 'local' && process.env.NODE_ENV !== 'test' && process.env.INSTANA_ENDPOINT_URL) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
+  require('@instana/collector')();
+}
+
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import Fastify from 'fastify-compress';
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
-
-if (process.env.NODE_ENV !== 'local' && process.env.NODE_ENV !== 'test' && process.env.INSTANA_ENDPOINT_URL) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
-  require('@instana/collector')();
-}
 
 const configService = new ConfigService();
 
