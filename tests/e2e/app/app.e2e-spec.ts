4274123ec58618ec7f8190633bd1134dcdfac6d4
Nikola Kovacevic <nikolakovacevic.zg@gmail.com>
2021-04-23 11:20:12 +0200

    fix(lint): fixes linting issue in app e2e test

--- tests/e2e/app/app.e2e-spec.ts
+++ tests/e2e/app/app.e2e-spec.ts
@@ -1,15 +1,8 @@
 import request from 'supertest';
 
 describe('AppController (e2e)', () => {
-  it('/ (GET)', async () =>
-    // has to be disabled as Prettier moves this to new line, maybe a better ESLint config is needed
-    // eslint-disable-next-line implicit-arrow-linebreak
-    request(global.app.getHttpServer()).get('/').expect(200));
-
-  it('Returns healthy graphQL status', async () => {
-    const res = await request(global.app.getHttpServer()).get(
-      '/.well-known/apollo/server-health',
-    );
-    expect(res['res'].statusCode).toBe(200); // eslint-disable-line
+  it('/ (GET)', async () => {
+    const response = await request(global.app.getHttpServer()).get('/');
+    expect(response['res'].statusCode).toEqual(200); // eslint-disable-line
   });
 });
