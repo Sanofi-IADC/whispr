ec79d1765c88585c28aadf308ad32e3da4e73171
Nikola Kovacevic <nikolakovacevic.zg@gmail.com>
2021-04-23 11:07:40 +0200

    test(e2e): fixes e2e tests for AppController

--- tests/e2e/app/app.e2e-spec.ts
+++ tests/e2e/app/app.e2e-spec.ts
@@ -1,9 +1,15 @@
 import request from 'supertest';
 
 describe('AppController (e2e)', () => {
-  it('/ (GET)', async () => request(global.app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
+  it('/ (GET)', async () =>
+    // has to be disabled as Prettier moves this to new line, maybe a better ESLint config is needed
+    // eslint-disable-next-line implicit-arrow-linebreak
+    request(global.app.getHttpServer()).get('/').expect(200));
+
   it('Returns healthy graphQL status', async () => {
-    const res = await request(global.app.getHttpServer()).get('/.well-known/apollo/server-health');
+    const res = await request(global.app.getHttpServer()).get(
+      '/.well-known/apollo/server-health',
+    );
     expect(res['res'].statusCode).toBe(200); // eslint-disable-line
   });
 });
