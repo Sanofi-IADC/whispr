64cebc9684efb02547cc914737e12739d70d07a3
Nikola Kovacevic <nikolakovacevic.zg@gmail.com>
2021-04-23 08:57:51 +0200

    test(unit): adds unit tests for appControler

--- tests/unit/app.controller.spec.ts
+++ tests/unit/app.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppController } from '../../src/app.controller';
 import { AppService } from '../../src/app.service';
+
+import { name, version } from '../../package.json';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +18,23 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return correct application version', () => {
+      expect(appController.getHello()).toHaveProperty('version');
+      expect(appController.getHello()).toMatchObject({ version });
+    });
+  });
+
+  describe('root', () => {
+    it('should return correct application name', () => {
+      expect(appController.getHello()).toHaveProperty('name');
+      expect(appController.getHello()).toMatchObject({ name });
+    });
+  });
+
+  describe('root', () => {
+    it('should return random haiku as an array', () => {
+      expect(appController.getHello()).toHaveProperty('haiku');
+      expect(Array.isArray(appController.getHello().haiku)).toBeTruthy();
     });
   });
 });
