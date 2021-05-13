14655fa3fed3daafbb29bf5f4b9caec0e899ed32
MeStrak <call_me_nigel@yahoo.co.uk>
2021-05-13 22:26:35 +0000

    style: cleanup file and comments

--- tests/unit/whisp/whisp.service.spec.ts
+++ tests/unit/whisp/whisp.service.spec.ts
@@ -1,49 +1,22 @@
 import { Logger } from '@nestjs/common';
-import { getConnectionToken, getModelToken, MongooseModule } from '@nestjs/mongoose';
+import { getModelToken } from '@nestjs/mongoose';
 import { Test } from '@nestjs/testing';
-import dotenv from 'dotenv';
-import { Connection, deleteModel, Model, MongooseDocument, Types, mongo } from 'mongoose';
+import { Connection, Model } from 'mongoose';
 import { IWhisp } from 'src/interfaces/whisp.interface';
 import { EventService } from '../../../src/event/event.service';
 import { FileService } from '../../../src/file/file.service';
 import { SequenceService } from '../../../src/sequence/sequence.service';
-import { whispSchema } from '../../../src/whisp/whisp.schema';
 import { WhispService } from '../../../src/whisp/whisp.service';
 import { DistributionService } from '../../../src/distribution/distribution.service';
-import { closeInMongodConnection, rootMongooseTestModule } from './helpers';
-
 
 jest.mock('../../../src/distribution/distribution.service');
 jest.mock('../../../src/event/event.service');
 jest.mock('../../../src/file/file.service');
 jest.mock('../../../src/sequence/sequence.service');
 
-// const INPUT_WHISP: IWhisp = {
-//   readableID: 'myid',
-//   type: 'mytype',
-//   severity: 0,
-//   description: 'desc',
-//   closed: true,
-//   applicationID: 'APP',
-//   plantID: 'P2',
-//   locationID: 'LOC_1',
-//   manual: true,
-//   openedBy: 'me',
-//   openedById: 'me123',
-//   closedBy: 'me',
-//   closedById: 'me123',
-//   timestamp: new Date(),
-//   updated: new Date(),
-//   data: {},
-//   tags: [],
-//   attachments: []
-// }
-
-dotenv.config({ path: 'test.env' });
 describe('WhispService', () => {
   let whispService: WhispService;
   let whispModel: Model<IWhisp>;
-  let dbConnection: Connection;
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [
@@ -53,16 +26,10 @@ describe('WhispService', () => {
       providers: [
         {
           provide: getModelToken('Whisp'),
-          // notice that only the functions we call from the model are mocked
           useFactory: () => ({
-            // new: jest.fn().mockResolvedValue(mockCat()),
-            // constructor: jest.fn().mockResolvedValue(mockCat()),
-            find: jest.fn(),
-            findOne: jest.fn(),
             findOneAndUpdate: jest.fn().mockReturnThis(),
             update: jest.fn(),
             create: jest.fn(),
-            remove: jest.fn(),
             aggregate: jest.fn().mockReturnThis(),
             exec: jest.fn(),
           }),
@@ -76,31 +43,19 @@ describe('WhispService', () => {
     }).compile();
     whispService = moduleRef.get<WhispService>(WhispService);
     whispModel = moduleRef.get<Model<IWhisp>>(getModelToken('Whisp'));
-    // dbConnection = moduleRef.get<Connection>(getConnectionToken());
-    // await whispModel.deleteMany({});
   });
-  afterEach(async () => {
-    // await closeInMongodConnection();
-  });
-  afterAll(async () => {
+  
 
-    // dbConnection.modelNames().forEach((modelName) => {
-    //   deleteModel(modelName);
-    // });
-    // await closeInMongodConnection();
-
-  });
   describe('create Whisp', () => {
     it('should set Timestamp when no timestamp is provided', async () => {
-      const result = await whispService.create({});
+      await whispService.create({});
 
       expect(whispModel.create).toBeCalledWith(
         expect.objectContaining({
           timestamp: expect.any(Date)
         }));
-
-      // expect(result.timestamp instanceof Date).toBeTruthy();
     });
+
     it('should keep custom timestamp when timestamp is provided', async () => {
       const timestamp = new Date();
       await whispService.create({ timestamp });
@@ -109,10 +64,9 @@ describe('WhispService', () => {
         expect.objectContaining({
           timestamp: timestamp
         }));
-
-      // expect(result.timestamp.valueOf()).toEqual(timestamp.valueOf());
     });
   });
+
   describe('Update Whisp', () => {
     it('should update timestamp when it is provided', async () => {
       const timestamp = new Date();
@@ -123,9 +77,8 @@ describe('WhispService', () => {
         expect.objectContaining({
           timestamp: timestamp
         }), expect.anything());
-
-      // expect(result.timestamp.valueOf()).toEqual(timestamp.valueOf());
     });
+
     it('should keep timestamp when it is not provided', async () => {
       await whispService.update('56cb91bdc3464f14678934ca', {});
       expect(whispModel.findOneAndUpdate).toBeCalledWith(expect.anything(),
@@ -201,10 +154,7 @@ describe('WhispService', () => {
         "applicationID": "SMUDGE",
         "data.customData.id": "504"
       }];
-
-
       const groupParam = { "mainGrouping": "$data.customData.id", "secondaryGrouping": "$data.customData.description" };
-
       await whispService.countWhispsGroup(matchParam, groupParam);
 
       const expectedMatch = {
