d04df8c78902c97e4c2e38eda9d5d41764f95898
MeStrak <call_me_nigel@yahoo.co.uk>
2022-02-04 23:58:13 +0000

    style: fix linting errors

--- tests/e2e/auth/auth.e2e-spec.ts
+++ tests/e2e/auth/auth.e2e-spec.ts
@@ -1,12 +1,12 @@
 import request from 'supertest';
 
 // JWT signed with key='VALID_SECRET KEY', expires in the year 2999 - the key should be updated before that
-const JWT_VALID_SECRET_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2NDQwMTE3NTksImV4cCI6MzI0NzUxNjA1ODksImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJHaXZlbk5hbWUiOiJKb2hubnkiLCJTdXJuYW1lIjoiUm9ja2V0IiwiRW1haWwiOiJqcm9ja2V0QGV4YW1wbGUuY29tIiwiUm9sZSI6WyJNYW5hZ2VyIiwiUHJvamVjdCBBZG1pbmlzdHJhdG9yIl19.uU2m8A1nz1qYjp_ncDCSxhRmqF-qkQ1Gcs8jo5-vO-o'
+const JWT_VALID_SECRET_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE2NDQwMTE3NTksImV4cCI6MzI0NzUxNjA1ODksImF1ZCI6Ind3dy5leGFtcGxlLmNvbSIsInN1YiI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJHaXZlbk5hbWUiOiJKb2hubnkiLCJTdXJuYW1lIjoiUm9ja2V0IiwiRW1haWwiOiJqcm9ja2V0QGV4YW1wbGUuY29tIiwiUm9sZSI6WyJNYW5hZ2VyIiwiUHJvamVjdCBBZG1pbmlzdHJhdG9yIl19.uU2m8A1nz1qYjp_ncDCSxhRmqF-qkQ1Gcs8jo5-vO-o'; // eslint-disable-line max-len
 const WHISPS_QUERY = `query getWhisps {
                         whispsAuthBeta(limit: 5) {
                           _id
                         }
-                      }`
+                      }`;
 
 describe('Authentication E2E tests', () => {
   describe('query whisps', () => {
@@ -26,8 +26,8 @@ describe('Authentication E2E tests', () => {
         .send({
           query: WHISPS_QUERY,
         });
-      expect(result.body.error).toBeUndefined;
-      expect(result.body.data).toBeTruthy;
+      expect(result.body.error).toBeUndefined();
+      expect(result.body.data).toBeTruthy();
     });
   });
 });
