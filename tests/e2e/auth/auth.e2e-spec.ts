112317643aa268a9a66d806c548a37dd07eed4c7
MeStrak <call_me_nigel@yahoo.co.uk>
2022-02-04 17:19:55 +0000

    test: add first auth e2e test

---
+++ tests/e2e/auth/auth.e2e-spec.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+
+describe('GRAPHQL WhispModule (e2e)', () => {
+    describe('createWhisp', () => {
+        it('should return 401 Unauthorized when request has no JWT', async () => {
+            const result = await request(global.app.getHttpServer())
+                .post('/graphql')
+                .send({
+                    query: `query getWhisps {
+                            whisps_authBeta(limit: 5) {
+                            _id
+                            }
+                        }`
+                });
+            expect(result.body.errors[0].extensions.response.statusCode).toBe(401);
+
+        })
+    })
+});
