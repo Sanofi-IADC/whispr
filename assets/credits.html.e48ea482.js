d74b2968e5c71fc3f78728729cdb6e7d839538f9
sairam459 <sairam459@users.noreply.github.com>
2024-06-04 11:49:12 +0000

    deploy: 41bafad4342ab8b825b3f550e89c664cfef9bd67

---
+++ assets/credits.html.e48ea482.js
@@ -0,0 +1,1 @@
+const t={key:"v-141584aa",path:"/contributing/credits.html",title:"Credits",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:171750173e4,contributors:[{name:"sairam charan",email:"mohanvenkat459@gmail.com",commits:1}]},filePathRelative:"contributing/credits.md"};export{t as data};
