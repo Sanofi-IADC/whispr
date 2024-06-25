68bf5cb299544b0a4cfc662521dd5ffd84f4808c
sairam459 <sairam459@users.noreply.github.com>
2024-06-25 08:51:27 +0000

    deploy: 725b276af40d6d9fa32f7fac937089085b68b67a

---
+++ assets/guidelines.html.46c62868.js
@@ -0,0 +1,1 @@
+const e={key:"v-7e30aeec",path:"/contributing/guidelines.html",title:"Guidelines",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1719305468e3,contributors:[{name:"sairam charan",email:"mohanvenkat459@gmail.com",commits:1}]},filePathRelative:"contributing/guidelines.md"};export{e as data};
