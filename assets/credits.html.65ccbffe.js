4b0c0d161f14c06d55b02731fc1a72d4ffc69741
krystiankedra <krystiankedra@users.noreply.github.com>
2024-08-23 07:38:31 +0000

    deploy: bb2cb7252f3209fdf3a01e7fb526bebc01fa3dd1

---
+++ assets/credits.html.65ccbffe.js
@@ -0,0 +1,1 @@
+const t={key:"v-141584aa",path:"/contributing/credits.html",title:"Credits",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1724398685e3,contributors:[{name:"Krystian K\u0119dra",email:"kedrakrystian@gmail.com",commits:1}]},filePathRelative:"contributing/credits.md"};export{t as data};
